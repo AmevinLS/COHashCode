928b501bd62f52ee9aaebdafb75b772e19647f82
siemieniuk <szymon.siemieniuk@akai.org.pl>
2023-01-04 14:02:57 +0100

    __name__ == '__main__' and cleanup

--- main.py
+++ main.py
@@ -1,34 +1,14 @@
-import numpy as np
+if __name__ == '__main__':
+    import numpy as np
 
-FILE = 'resources/a_example.txt'
+    from reader import read_file
 
-with open(FILE, 'r', encoding='utf-8') as f:
-    B, L, D = map(int, f.readline().split(' '))
-    n = np.zeros(L, dtype=np.uintc)
-    t = np.zeros(L, dtype=np.uintc)
-    m = np.zeros(L, dtype=np.uintc)
-    libraries = []
+    FILE = 'resources/a_example.txt'
 
-    params = tuple(map(int, f.readline().split(' ')))
-    s = np.array(params, dtype=np.uintc)
+    n, t, m, libraries, s = read_file(FILE)
 
-    is_odd = True
-    i = 0
-    for line in f:
-        if is_odd:
-            n[i], t[i], m[i] = map(int, line.split(' '))
-        else:
-            params = tuple(map(int, line.split(' ')))
-            libraries.append(np.array(params, dtype=np.uintc))
-            i += 1
-        is_odd = not is_odd
-
-del params
-del is_odd
-del i
-
-print(n)
-print(t)
-print(m)
-for library in libraries:
-    print(library)
+    print(n)
+    print(t)
+    print(m)
+    for library in libraries:
+        print(library)
